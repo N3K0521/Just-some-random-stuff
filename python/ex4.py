20da3fec46475944be75a28d6648332c9811c1fd
N3K0521 <wanghuixin0521@gmail.com>
2021-07-10 02:56:09 +0800

    enter

---
+++ python/ex4.py
@@ -0,0 +1,19 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
+# difference btw "=" and "==":
+# "=": 将右边的值，赋给左边的变量名 （set the value on the right side to the variable on the left, set the left equal to right.)
+# "==": check whether the values on the two sides are equal
